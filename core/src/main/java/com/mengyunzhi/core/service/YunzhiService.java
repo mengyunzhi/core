a0a4c1f722a60c6c7799276da2307e7066ff8be2
teacherPan <3792535@qq.com>
2018-08-04 15:59:01 +0800

    修正注释

--- core/src/main/java/com/mengyunzhi/core/service/YunzhiService.java
+++ core/src/main/java/com/mengyunzhi/core/service/YunzhiService.java
@@ -13,15 +13,17 @@ import java.util.List;
 public interface YunzhiService {
     /**
      * 通过传入的实体进行多条件查询
+     * @param jpaSpecificationExecutor jpa多查询仓库
      * @param entity 实体
      * @param pageable 分页信息
-     * @return
+     * @return 分页信息
      * panjie
      */
     Page<?> page(JpaSpecificationExecutor jpaSpecificationExecutor, Object entity, Pageable pageable);
 
     /**
      * 通过传入的实体查询所有数据
+     * @param jpaSpecificationExecutor jpa多查询仓库
      * @param entity 实体
      * @return
      * panjie
