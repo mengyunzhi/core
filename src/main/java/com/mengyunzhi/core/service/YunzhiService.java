1c09f62bfd99389cdb08cbd638c68fc0dde917d3
panjie <3792535@qq.com>
2019-01-25 22:30:54 +0800

    大范围重写，并引用泛型

--- src/main/java/com/mengyunzhi/core/service/YunzhiService.java
+++ src/main/java/com/mengyunzhi/core/service/YunzhiService.java
@@ -1,6 +1,5 @@
 package com.mengyunzhi.core.service;
 
-import com.mengyunzhi.core.entity.YunzhiEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -11,7 +10,7 @@ import java.util.List;
  * @author panjie
  */
 public interface YunzhiService<T> {
-    Page<T> page(JpaSpecificationExecutor jpaSpecificationExecutor, YunzhiEntity entity, Pageable pageable);
+    Page<T> page(JpaSpecificationExecutor jpaSpecificationExecutor, T entity, Pageable pageable);
 
-    List<T> findAll(JpaSpecificationExecutor jpaSpecificationExecutor, YunzhiEntity entity);
+    List<T> findAll(JpaSpecificationExecutor jpaSpecificationExecutor, T entity);
 }
