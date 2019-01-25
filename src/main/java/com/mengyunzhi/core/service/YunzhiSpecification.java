a55198f251a7dc3dd6b3bc012b74b5c0b68c1c79
panjie <3792535@qq.com>
2019-01-25 22:35:47 +0800

    将Specificatin分离，并引入单元测试

---
+++ src/main/java/com/mengyunzhi/core/service/YunzhiSpecification.java
@@ -0,0 +1,260 @@
+package com.mengyunzhi.core.service;
+
+import com.mengyunzhi.core.annotation.query.GreaterThanOrEqualTo;
+import com.mengyunzhi.core.annotation.query.Ignore;
+import com.mengyunzhi.core.annotation.query.In;
+import com.mengyunzhi.core.annotation.query.LessThanOrEqualTo;
+import com.mengyunzhi.core.entity.YunzhiEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.Embeddable;
+import javax.persistence.criteria.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.Collection;
+
+/**
+ * @author panjie
+ * 综合查询构造器
+ */
+public class YunzhiSpecification<O> implements Specification<O> {
+    private final static Logger logger = LoggerFactory.getLogger(YunzhiSpecification.class);
+    private Predicate predicate = null;
+    private CriteriaBuilder criteriaBuilder;
+    private O entity;       // 实体
+
+    public YunzhiSpecification(O entity) {
+        this.entity = entity;
+    }
+
+    /**
+     * 设置and谓语.注意，这里只能设置and关系的谓语，如果谓语为OR，则需要手动设置
+     * @param predicate 查询谓语
+     */
+    private void andPredicate(Predicate predicate) {
+        if (null != predicate) {
+            if (null == this.predicate) {
+                this.predicate = predicate;
+            } else {
+                this.predicate = this.criteriaBuilder.and(this.predicate, predicate);
+            }
+        }
+    }
+
+    private void generatePredicate(Object entity, From<O, ?> root) {
+        logger.debug("反射字段，按字段类型，进行综合查询");
+        Field[] fields = entity.getClass().getDeclaredFields();
+        try {
+            for (Field field : fields) {
+                logger.debug("设置字段可见，并获取实体值");
+                field.setAccessible(true);
+                Object value = field.get(entity);
+
+                if (value != null) {
+                    if (Modifier.isFinal(field.getModifiers())) {
+                        logger.debug("字段类型为final");
+                        continue;
+                    }
+
+                    if (Modifier.isStatic(field.getModifiers())) {
+                        logger.debug("字段类型为static");
+                        continue;
+                    }
+
+                    String name = field.getName();
+                    if (name.equals("serialVersionUID")) {
+                        logger.debug("字段名为serialVersionUID");
+                        continue;
+                    }
+
+                    if (field.getAnnotation(Ignore.class) != null) {
+                        logger.debug("存在@Ignore注解, 跳出");
+                        continue;
+                    }
+
+                    if (this.beginOrEndQuery(root, value, field)) {
+                        logger.debug("执行了大于等于或是小于等于查询");
+                        continue;
+                    }
+
+                    if (this.inQuery(root, field, value)) {
+                        logger.debug("执行了范围查询");
+                        continue;
+                    }
+
+                    // 按字段类型进行查询
+                    if (value instanceof Boolean) {
+                        logger.debug("布尔值");
+                        Boolean booleanValue = ((Boolean) value);
+                        this.andPredicate(criteriaBuilder.equal(root.get(name).as(Boolean.class), booleanValue));
+                    } else if (value instanceof String) {
+                        logger.debug("字符串则进行模糊查询");
+                        String stringValue = ((String) value);
+                        if (!stringValue.isEmpty()) {
+                            this.andPredicate(criteriaBuilder.like(root.get(name).as(String.class), "%" + stringValue + "%"));
+                        }
+                    } else if (value instanceof Number || value instanceof Calendar || value instanceof Date) {
+                        logger.debug("如果为number，则进行精确或范围查询");
+                        this.andPredicate(criteriaBuilder.equal(root.get(name).as(value.getClass()), value));
+                    } else if (value instanceof YunzhiEntity) {
+                        logger.debug("是实体类");
+                        YunzhiEntity yunZhiEntity = (YunzhiEntity) value;
+                        if (yunZhiEntity.getId() != null) {
+                            logger.debug("对应的ManyToOne，加入了id, 则按ID查询");
+                            this.andPredicate(criteriaBuilder.equal(root.join(name).get("id").as(Long.class), yunZhiEntity.getId()));
+                        } else {
+                            logger.debug("未加入id, 则进行Join查询");
+                            this.generatePredicate(value, root.join(name));
+                        }
+
+                    } else if (value instanceof Collection) {
+                        Collection<?> collectionValue = (Collection<?>) value;
+                        if (collectionValue.size() > 0) {
+                            logger.warn("暂不支持一对多，多对多查询");
+                            // todo: 一对多，多对多查询
+                        }
+                    } else {
+                        Class<?> clazz = value.getClass();
+                        if (clazz.isAnnotationPresent(Embeddable.class)) {
+                            logger.debug("为内部Embeddable");
+                            this.generatePredicate(value, root.join(name));
+                        } else {
+                            logger.error("综合查询暂不支持传入的数据类型:" + name + "->" + field.toString() + " " + field.getClass().getName());
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * in 查询
+     * @param root 根查询实体
+     * @param field 字段
+     * @param value 值
+     * @return 进行了in直询，true;未进行in查询,false.
+     */
+    private Boolean inQuery(From<O, ?> root, Field field, Object value) {
+        // 获取in注解
+        In inQueryParam = field.getAnnotation(In.class);
+        if (inQueryParam != null) {
+            logger.debug("存在@InQueryParam注解");
+            String inQueryName = inQueryParam.name();
+            /*
+             * In查询相关代码
+             * https://blog.csdn.net/jiangyu1013/article/details/79056231
+             * Expression：查询表达式
+             * 源码中支持isNull，isNotNull，in，返回值为根据表达式生成的谓语
+             */
+            logger.debug("执行In查询");
+            try {
+                Collection<?> collectionValue = (Collection<?>) value;
+                Expression<Object> expression = root.get(inQueryName);
+                this.andPredicate(expression.in(collectionValue));
+            } catch (ClassCastException e) {
+                String message = "将对象转换为Collention时发生错误。注意：in查询只能做用到Collention中";
+                if (value != null) {
+                    message += "->" + value.getClass().getName();
+                }
+                logger.warn(message);
+                e.printStackTrace();
+            }
+        }
+        return inQueryParam != null;
+    }
+
+    /**
+     * 范围查询 查询大于等于或是小于等于
+     * @param root 基实体
+     * @param value 值
+     * @param field 字段
+     * @return Boolean 是否执行了范围查询
+     *
+     */
+    private Boolean beginOrEndQuery(From<O, ?> root, Object value, Field field) {
+        String name = field.getName();
+        // 初始化两个界限的变量
+        Boolean isBegin = false;
+        Boolean isEnd = false;
+
+        // 查找开始与结束的注解
+        GreaterThanOrEqualTo beginQueryParam = field.getAnnotation(GreaterThanOrEqualTo.class);
+        if (beginQueryParam != null) {
+            logger.debug("存在@BeginQueryParam注解, 进行大于等于查询");
+            isBegin = true;
+            name = beginQueryParam.name();
+        } else if (field.getAnnotation(LessThanOrEqualTo.class) != null) {
+            logger.debug("存在@EndQueryParam注解");
+            isEnd = true;
+            name = field.getAnnotation(LessThanOrEqualTo.class).name();
+        }
+
+        if (isEnd || isBegin) {
+            if (value instanceof Number) {
+                logger.debug("传入类型为number");
+                if (value instanceof Byte) {
+                    Byte byteValue = (Byte) value;
+                    if (isBegin) {
+                        this.andPredicate(criteriaBuilder.greaterThanOrEqualTo(root.get(name).as(Byte.class), byteValue));
+                    } else {
+                        this.andPredicate(criteriaBuilder.lessThanOrEqualTo(root.get(name).as(Byte.class), byteValue));
+                    }
+                } else if (value instanceof Short) {
+                    Short shortValue = (Short) value;
+                    if (isBegin) {
+                        this.andPredicate(criteriaBuilder.greaterThanOrEqualTo(root.get(name).as(Short.class), shortValue));
+                    } else {
+                        this.andPredicate(criteriaBuilder.lessThanOrEqualTo(root.get(name).as(Short.class), shortValue));
+                    }
+                } else if (value instanceof Integer) {
+                    Integer integerValue = (Integer) value;
+                    if (isBegin) {
+                        this.andPredicate(criteriaBuilder.greaterThanOrEqualTo(root.get(name).as(Integer.class), integerValue));
+                    } else {
+                        this.andPredicate(criteriaBuilder.lessThanOrEqualTo(root.get(name).as(Integer.class), integerValue));
+                    }
+                } else if (value instanceof Long) {
+                    Long longValue = (Long) value;
+                    if (isBegin) {
+                        this.andPredicate(criteriaBuilder.greaterThanOrEqualTo(root.get(name).as(Long.class), longValue));
+                    } else {
+                        this.andPredicate(criteriaBuilder.lessThanOrEqualTo(root.get(name).as(Long.class), longValue));
+                    }
+                } else {
+                    logger.error("大于等于、小于等于中的Number类型，暂时只支持到Byte, Short,Integer,Long");
+                }
+            } else if (value instanceof Calendar) {
+                Calendar calendarValue = (Calendar) value;
+                if (isBegin) {
+                    this.andPredicate(criteriaBuilder.greaterThanOrEqualTo(root.get(name).as(Calendar.class), calendarValue));
+                } else {
+                    this.andPredicate(criteriaBuilder.lessThanOrEqualTo(root.get(name).as(Calendar.class), calendarValue));
+                }
+            } else if (value instanceof Date) {
+                logger.debug("Sql.Date类型");
+                Date dateValue = (Date) value;
+                if (isBegin) {
+                    this.andPredicate(criteriaBuilder.greaterThanOrEqualTo(root.get(name).as(Date.class), dateValue));
+                } else {
+                    this.andPredicate(criteriaBuilder.lessThanOrEqualTo(root.get(name).as(Date.class), dateValue));
+                }
+            }
+        }
+
+        return isBegin || isEnd;
+    }
+
+    @Override
+    public Predicate toPredicate(Root<O> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+        // 设置CriteriaBuilder，用于合并谓语
+        this.criteriaBuilder = criteriaBuilder;
+        this.generatePredicate(this.entity, root);
+        return this.predicate;
+    }
+}
---
+++ test/src/test/java/com/mengyunzhi/core/test/YunzhiSpecificationTest.java
@@ -0,0 +1,74 @@
+package com.mengyunzhi.core.test;
+
+import com.mengyunzhi.core.service.YunzhiSpecification;
+import com.mengyunzhi.core.test.entity.Klass;
+import com.mengyunzhi.core.test.repository.KlassRepository;
+import com.mengyunzhi.core.test.service.KlassService;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+/**
+ * @author panjie
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class YunzhiSpecificationTest {
+    @Autowired
+    KlassService klassService;
+    @Autowired
+    KlassRepository klassRepository;
+
+    @Test
+    public void generatePredicate() {
+        Klass originKlass = klassService.getOneSavedKlass();
+        Klass klass = new Klass();
+        PageRequest pageable = PageRequest.of(0, 2);
+
+        klass.setId(-1L);
+        Specification specification = KlassSpecification.base(klass);
+        Page<Klass> klassPage = (Page<Klass>) this.klassRepository.findAll(specification, pageable);
+        Assertions.assertThat(klassPage.getTotalElements()).isEqualTo(0);
+
+        klass.setId(originKlass.getId());
+        specification = KlassSpecification.base(klass);
+        klassPage = (Page<Klass>) this.klassRepository.findAll(specification, pageable);
+        Assertions.assertThat(klassPage.getTotalElements()).isEqualTo(1);
+
+        klass.setId(originKlass.getId());
+        originKlass.setName("修改班级名称");
+        klassRepository.save(originKlass);
+        specification = KlassSpecification.base(klass);
+        klassPage = this.klassRepository.findAll(specification, pageable);
+        Assertions.assertThat(klassPage.getTotalElements()).isEqualTo(0);
+    }
+
+    public static class KlassSpecification {
+
+        public static Specification<Klass> base(Klass klass) {
+            return (Specification<Klass>) (root, criteriaQuery, criteriaBuilder) -> {
+                // 先获取YunzhiSpecification自动生成的查询条件
+                YunzhiSpecification<Klass> yunzhiSpecification = new YunzhiSpecification<>(klass);
+                Predicate predicate = yunzhiSpecification.toPredicate(root, criteriaQuery, criteriaBuilder);
+
+                // 再加入其它的查询条件。比如加入NAME查询
+                Predicate predicate1 = criteriaBuilder.like(root.get("name").as(String.class), "%测试班级名称%");
+                predicate = criteriaBuilder.and(predicate, predicate1);
+                return predicate;
+            };
+        }
+    }
+}
