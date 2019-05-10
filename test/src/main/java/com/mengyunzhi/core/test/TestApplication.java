1ac47ea4a615a86eba24ca0d140f5a857e58d0e3
panjie <3792535@qq.com>
2019-05-10 10:25:01 +0800

    增加httpAppender

--- test/src/main/java/com/mengyunzhi/core/test/TestApplication.java
+++ test/src/main/java/com/mengyunzhi/core/test/TestApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestApplication {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(TestApplication.class, args);
     }
-
 }
 
